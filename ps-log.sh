19bff30231a3526f7fde978fe6ac4f7bd86dc689
Alfonso Tienda <afoone@hotmail.com>
2022-05-05 12:21:05 +0200

    Update ps-log.sh
    prepared for crontab

--- ps-log.sh
+++ ps-log.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 touch /var/log/ps.log
 MaxFileSize=2048
-while true
-do
-    ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail >> /var/log/ps.log
-    #Get size in bytes** 
-    file_size=`du -b /var/log/ps.log | tr -s '\t' ' ' | cut -d' ' -f1`
-    if [ $file_size -gt $MaxFileSize ];then   
-        timestamp=`date +%s`
-        mv /var/log/ps.log /var/log/ps.log.$timestamp
-        touch /var/log/ps.log
-    fi
-done
+
+ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail >> /var/log/ps.log
+#Get size in bytes** 
+file_size=`du -b /var/log/ps.log | tr -s '\t' ' ' | cut -d' ' -f1`
+if [ $file_size -gt $MaxFileSize ];then   
+    timestamp=`date +%s`
+    mv /var/log/ps.log /var/log/ps.log.$timestamp
+    touch /var/log/ps.log
+fi
