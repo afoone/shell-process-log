48c6f60e2a9793db41ad1007ce831445a573aaff
Alfonso Tienda <afoone@hotmail.com>
2022-05-05 12:34:36 +0200

    dont cut information

--- ps-log.sh
+++ ps-log.sh
@@ -2,7 +2,7 @@
 touch /var/log/ps.log
 MaxFileSize=2048
 echo -e "\n Fecha:"`date` >> /var/log/ps.log
-ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail >> /var/log/ps.log
+ps -e -o pcpu,pmem,args --sort=pcpu | tail >> /var/log/ps.log
 #Get size in bytes** 
 file_size=`du -b /var/log/ps.log | tr -s '\t' ' ' | cut -d' ' -f1`
 if [ $file_size -gt $MaxFileSize ];then   
