b2375200989eb33bed2fff34a7931486bc2b7e3c
Alfonso Tienda <afoone@hotmail.com>
2022-05-05 13:00:07 +0200

    error on gzip location

--- ps-log.sh
+++ ps-log.sh
@@ -9,7 +9,7 @@ file_size=`du -b /var/log/ps.log | tr -s '\t' ' ' | cut -d' ' -f1`
 if [ $file_size -gt $MaxFileSize ];then   
     timestamp=`date +%s`
     mv /var/log/ps.log /var/log/ps.log.$timestamp
-    gzip ps.log.$timestamp
+    gzip /var/log/ps.log.$timestamp
     touch /var/log/ps.log
     # remove old files
     find /var/log -name "ps.log.*" -type f -mtime +$DaysToKeep -delete 
