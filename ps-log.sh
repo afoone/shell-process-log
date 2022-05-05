40fbc3bf3a3bf367ecf017613f6a2c861f89817a
Alfonso Tienda <afoone@hotmail.com>
2022-05-05 12:45:44 +0200

    gzip and delete old files

--- ps-log.sh
+++ ps-log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-touch /var/log/ps.log
 MaxFileSize=2048
+DaysToKeep=7
 echo -e "\n Fecha:"`date` >> /var/log/ps.log
 ps -e -o pcpu,pmem,args --sort=pcpu | tail >> /var/log/ps.log
 #Get size in bytes** 
@@ -8,5 +8,8 @@ file_size=`du -b /var/log/ps.log | tr -s '\t' ' ' | cut -d' ' -f1`
 if [ $file_size -gt $MaxFileSize ];then   
     timestamp=`date +%s`
     mv /var/log/ps.log /var/log/ps.log.$timestamp
+    gzip ps.log.$timestamp
     touch /var/log/ps.log
+    # remove old files
+    find /var/log -name "ps.log.*" -type f -mtime +$DaysToKeep -delete 
 fi
